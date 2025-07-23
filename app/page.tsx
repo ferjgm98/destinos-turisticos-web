82fb977879aaac8cec819e18a82bb0c19f389ce0
Fernando González <fernandomenjivar2011@gmail.com>
2025-07-22 21:30:13 -0600

    style: add home image width to cover on resize

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
+import HeroImage from "/images/hero.png";
 
 export default function Home() {
   return (
     <div className="">
-      <Image src="/images/hero.png" width={1920} height={800} alt="hero" />
-      <Image src="/images/band.png" width={1920} height={77} alt="band" />
+      <Image
+        src="/images/hero.png"
+        width={1920}
+        height={800}
+        alt="hero"
+        className="w-full object-cover"
+      />
+      <Image
+        src="/images/band.png"
+        width={1920}
+        height={77}
+        className="w-full object-cover"
+        alt="band"
+      />
     </div>
   );
 }
