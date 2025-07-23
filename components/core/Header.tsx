6430bb55519b0be2bdd03c1631b4796b470abae9
Fernando González <fernandomenjivar2011@gmail.com>
2025-07-22 21:21:38 -0600

    feat: add dynamic button  props functionality based on pathname

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -2,22 +2,37 @@
 
 import Image from "next/image";
 import Button from "./Button";
+import { usePathname, useRouter } from "next/navigation";
+import { useMemo } from "react";
 
 export default function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const buttonProps = useMemo(() => {
+    if (pathname === "/") {
+      return {
+        label: "Agregar destino",
+        onClick: () => router.push("/agregar-destino"),
+      };
+    }
+
+    return {
+      label: "Listado de destinos",
+      onClick: () => router.push("/"),
+    };
+  }, [pathname]);
+
   return (
     <div className="py-4 px-4 flex justify-between items-center">
       <Image
         src="/images/logo.png"
         alt="logo El Salvador"
-        width={502}
+        width={402}
         height={81}
       />
 
-      <Button
-        label="Agregar destino"
-        onClick={() => alert("hola")}
-        className="px-16"
-      />
+      <Button {...buttonProps} className="px-16 cursor-pointer" />
     </div>
   );
 }
